c4e8dbbfaaa18fcd9177579c86fdb70756ab4e69
StefanClausen <stefanclaussen@me.com>
2016-04-24 19:34:27 +0100

    Section complete and is now ready to start UITableView's Data source section

---
+++ BNRHomepwner/BNRHomepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  BNRHomepwner
+//
+//  Created by Stefan Claussen on 24/04/2016.
+//  Copyright © 2016 One foot after the other. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
